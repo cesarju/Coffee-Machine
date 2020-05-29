3017ee308e2bbaceea8c4cef6a25c00358e46a56
Cesar <cesarju.cc@gmail.com>
2020-05-29 12:53:29 -0400

    Epata 3 terninada

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -12,7 +12,7 @@ fun main() {
     println("Pouring coffee into the cup")
     println("Pouring some milk into the cup")
     println("Coffee is ready!")
-    */
+
     //Etapa 2/6
 
     val scanner = Scanner(System.`in`)
@@ -22,4 +22,34 @@ fun main() {
     println("${nroCafe * 200} ml of water")
     println("${nroCafe * 50} ml of milk")
     println("${nroCafe * 15} g of coffee beans")
+    */
+    //Etapa 3/6
+
+    val scanner = Scanner(System.`in`)
+    print("Write how many ml of water the coffee machine has: > ")
+    val nroWatter = scanner.nextInt()
+    print("Write how many ml of milk the coffee machine has: > ")
+    val nroMilk = scanner.nextInt()
+    print("Write how many grams of coffee beans the coffee machine has: > ")
+    val nroCoffe = scanner.nextInt()
+    print("Write how many cups of coffee you will need: > ")
+    val cant = scanner.nextInt()
+
+    val cantW:Int = nroWatter/200
+    val cantM:Int = nroMilk/50
+    val cantC:Int =nroCoffe/15
+
+    if (cantW>=cant){
+        if (cantM>=cant) {
+            if (cantC>=cant) {
+                if (minOf(cantW,cantM,cantC)==cant){
+                    println("Yes, I can make that amount of coffee")
+                }else{
+                    println("Yes, I can make that amount of coffee (and even ${(minOf(cantW,cantM,cantC))-cant} more than that)")
+                }
+            }
+        }
+    }else{
+        println("No, I can make only ${minOf(cantW,cantM,cantC)} cups of coffee")
+    }
 }
